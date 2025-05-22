95704a8ceba6f39c9259971b45f2c67ebfce5335
Michael Gift <simiyugift@outlook.com>
2025-05-22 21:48:41 +0300

    Add DeliveryFee and Widgets data files

---
+++ src/data/DeliveryFee.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace src\data;
+
+return [
+    ['min' => 90, 'fee' => 0],
+    ['min' => 50, 'fee' => 2.95],
+    ['min' => 0, 'fee' => 4.95]
+];
---
+++ src/data/Widgets.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace src\data;
+
+return [
+    'R01' => 32.95,
+    'G01' => 24.95,
+    'B01' => 7.95
+];
